662523fdc0861bbc87af9d59a2aae7754c822bd6
Oliver Stenbom <oliver@stenbom.eu>
2025-03-26 11:04:47 +0100

    Format astro.config

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Endform Docs
 
 Repository for our documentation site.
+
+It's available at [endform.dev/docs](https://endform.dev/docs).
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/docs',
+  base: "/docs",
   integrations: [
-      starlight({
-          title: 'Endform Docs',
-          social: {
-              github: 'https://github.com/endformdev/docs',
-          },
-					customCss: [
-						'./src/styles/custom.css',
-					],
-          sidebar: [
-              {
-                  label: 'Guides',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
-                  ],
-              },
-              {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
-              },
+    starlight({
+      title: "Endform Docs",
+      social: {
+        github: "https://github.com/endformdev/docs",
+      },
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
           ],
-      }),
-	],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 
   adapter: cloudflare({
-			imageService: 'cloudflare',
-      platformProxy: {
-          enabled: true,
-					
-      }
+    imageService: "cloudflare",
+    platformProxy: {
+      enabled: true,
+    },
   }),
 });
