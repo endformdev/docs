98203833dc891f3572d0d6710f0c65041070fc26
Oliver Stenbom <oliver@stenbom.eu>
2025-05-06 13:22:02 +0200

    Update env var transfer description & deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,42 +5,46 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/docs",
-  integrations: [
-    starlight({
-      title: "Endform docs",
-      logo: {
-        dark: "./src/assets/endform-logo-wordmark-colored-on-dark-padding.svg",
-        light:
-          "./src/assets/endform-logo-wordmark-colored-on-light-padding.svg",
-        replacesTitle: true,
-        alt: "Endform logo",
-      },
-      social: {
-        github: "https://github.com/endformdev/docs",
-      },
-      customCss: ["./src/styles/custom.css"],
-      sidebar: [
-        {
-          label: "Explanation",
-          autogenerate: { directory: "explanation" },
-        },
-        {
-          label: "Guides",
-          autogenerate: { directory: "guides" },
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-  ],
+	base: "/docs",
+	integrations: [
+		starlight({
+			title: "Endform docs",
+			logo: {
+				dark: "./src/assets/endform-logo-wordmark-colored-on-dark-padding.svg",
+				light:
+					"./src/assets/endform-logo-wordmark-colored-on-light-padding.svg",
+				replacesTitle: true,
+				alt: "Endform logo",
+			},
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/endformdev/docs",
+				},
+			],
+			customCss: ["./src/styles/custom.css"],
+			sidebar: [
+				{
+					label: "Explanation",
+					autogenerate: { directory: "explanation" },
+				},
+				{
+					label: "Guides",
+					autogenerate: { directory: "guides" },
+				},
+				{
+					label: "Reference",
+					autogenerate: { directory: "reference" },
+				},
+			],
+		}),
+	],
 
-  adapter: cloudflare({
-    imageService: "passthrough",
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+	adapter: cloudflare({
+		imageService: "passthrough",
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 });
