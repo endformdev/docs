74c0fc69a4fb8b0791319d35c185579666ec18b8
Oliver Stenbom <oliver@stenbom.eu>
2025-08-29 10:59:40 +0200

    Add Playwright tutorial start (#1)
    Add the base for the playwright tutorial and most of stage one.

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,12 +35,16 @@ export default defineConfig({
 			],
 			sidebar: [
 				{
-					label: "Explanation",
-					autogenerate: { directory: "explanation" },
-				},
-				{
 					label: "Guides",
 					autogenerate: { directory: "guides" },
+				},
+				{
+					label: "Tutorial",
+					autogenerate: { directory: "tutorial" },
+				},
+				{
+					label: "Explanation",
+					autogenerate: { directory: "explanation" },
 				},
 				{
 					label: "Reference",
