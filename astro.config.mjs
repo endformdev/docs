f551e45e6ea3333626a08ac91561bbe71196b5e5
Jakob Norlin <jakob.stahl91@gmail.com>
2026-02-24 13:23:29 +0100

    Add Google Tag Manager

--- astro.config.mjs
+++ astro.config.mjs
@@ -63,6 +63,15 @@ export default defineConfig({
 			head: [
 				{
 					tag: "script",
+					// Google Tag Manager
+					content: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MG4F2GKF');`
+				},
+				{
+					tag: "script",
 					attrs: {
 						id: "posthog-js",
 						type: "text/javascript",
