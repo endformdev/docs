6105f756f0efe45cfd45c9a8bcda633e6e993385
Jakob Norlin <jakob.stahl91@gmail.com>
2026-04-29 15:22:15 +0200

    Add RB2B script

--- astro.config.mjs
+++ astro.config.mjs
@@ -164,6 +164,21 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 					})
 				`,
 							},
+							{
+								tag: "script",
+								attrs: {
+									type: "text/javascript",
+								},
+								content: `
+									!function(key) {
+										if (window.reb2b) return;
+										window.reb2b = {loaded: true};
+										var s = document.createElement("script");
+										s.async = true;
+										s.src = "https://ddwl4m2hdecbv.cloudfront.net/b/" + key + "/" + key + ".js.gz";
+										document.getElementsByTagName("script")[0].parentNode.insertBefore(s, document.getElementsByTagName("script")[0]);
+									}("VN080H3J986J");`,
+							},
 						],
 		}),
 	],
